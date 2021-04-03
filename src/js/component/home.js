f1824b780b62c3e673f7c8262d97dd2640e7c64e
vlmarq <lamarque.van@gmail.com>
2021-04-03 15:00:34 +0000

    Added clear list option

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,85 +1,91 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
-import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
 //include images into your bundle
 
 export function Home() {
-	// Creates todo list
-	const [list, setList] = useState([
-		{ label: "Walk the dog", isDone: false },
-		{ label: "Do some laundry", isDone: false },
-		{ label: "Finish up React projects", isDone: false }
-	]);
+    // Creates todo list
+    const [list, setList] = useState([
+        { label: "Walk the dog", isDone: false },
+        { label: "Do some laundry", isDone: false },
+        { label: "Finish up React projects", isDone: false }
+    ]);
 
-	// Creates todo item
-	const [todo, setTodo] = useState("");
+    // Creates todo item
+    const [todo, setTodo] = useState("");
 
-	// e = event | Function handles submit
-	const addTodo = e => {
-		// Prevents a browser’s default behavior for events
-		e.preventDefault();
+    // e = event | Function handles submit
+    const addTodo = e => {
+        if (e.key === "Enter" && todo !== "") {
+            setList(
+                list.concat({
+                    // Assigns todo value to label
+                    label: todo,
+                    isDone: false
+                })
+            );
 
-		setList([
-			// This stops the existing list from being overridden
-			...list,
-			{
-				// Assigns todo value to label
-				label: todo,
-				isDone: false
-			}
-		]);
+            // Resets todo back to empty string
+            setTodo("");
+        }
+    };
 
-		// Resets todo back to empty string
-		setTodo("");
-	};
+    const removeTodo = index => {
+        setList(list.filter((item, i) => index != i));
+    };
 
-	const removeTodo = e => {
-		const label = e.target.getAttribute("label");
-		setList(list.filter(item => item.label != label));
-	};
-
-	return (
-		<div className="d-flex flex-column align-items-center justify-content-center h-100">
-			<h1>To Do</h1>
-			{/* When enter is pressed, the function "addTodo" runs */}
-			<form onSubmit={addTodo}>
-				<ul className="list-unstyled d-flex flex-column">
-					<li>
-						<input
-							type="text"
-							placeholder="What has to be done?"
-							className="form-control"
-							value={todo}
-							/* When user changes content of input field, event will trigger setTodo */
-							onChange={e => setTodo(e.target.value)}
-						/>
-					</li>
-					{// Loop to create li
-					list.map((item, index) => {
-						return (
-							<li
-								key={index}
-								label={item.label}
-								className="d-flex listItem"
-								onClick={removeTodo}>
-								{item.label}{" "}
-								<span className="ml-auto">
-									<FontAwesomeIcon
-										className="icon"
-										icon={faTimes}
-									/>
-								</span>
-							</li>
-						);
-					})}
-					<li className="mt-3">
-						<span id="number">{list.length}</span> item
+    return (
+        <div className="d-flex flex-column align-items-center justify-content-center h-100">
+            <h1>To Do</h1>
+            {/* When enter is pressed, the function "addTodo" runs */}
+            <form onSubmit={e => e.preventDefault()}>
+                <ul className="list-unstyled d-flex flex-column p-0">
+                    <li>
+                        <input
+                            type="text"
+                            placeholder="What has to be done?"
+                            className="form-control"
+                            value={todo}
+                            /* When user changes content of input field, event will trigger setTodo */
+                            onChange={e => setTodo(e.target.value)}
+                            onKeyPress={e => addTodo(e)}
+                        />
+                    </li>
+                    {// Loop to create li
+                        list.map((item, index) => {
+                            return (
+                                <li key={index} className="d-flex listItem">
+                                    {item.label}
+                                    <span
+                                        className="ml-auto"
+                                        role="button"
+                                        tabIndex="0"
+                                        onClick={() => removeTodo(index)}>
+                                        <FontAwesomeIcon
+                                            className="icon"
+                                            icon={faTimes}
+                                        />
+                                    </span>
+                                </li>
+                            );
+                        })}
+                    <li className="mt-3 d-inline-flex align-items-center">
+                        <span id="number">{list.length}</span>
+						&nbsp;item
 						{list.length > 1 || list.length === 0 ? "s" : null} left
-					</li>
-				</ul>
-			</form>
-		</div>
-	);
+						<span className="ml-auto">
+                            <a
+                                href="#"
+                                className="btn btn-small btn-outline-info"
+                                // Messes up add todo and leaves one empty li
+                                onClick={() => setList([])}>
+                                Clear List
+							</a>
+                        </span>
+                    </li>
+                </ul>
+            </form>
+        </div>
+    );
 }
